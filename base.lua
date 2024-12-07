804e791545caaccea9e5b09a441f74d38b1261bd
mbien1981 <shieldhater@gmail.com>
2024-12-07 11:40:47 +0100

    Set version number to 1.4

--- base.lua
+++ base.lua
@@ -2,7 +2,7 @@ local module = DMod:new("loadout_dropdowns", {
 	name = "Loadout dropdowns",
 	description = "Allows you to open a dropdown gui out of your kit selection menu nodes",
 	author = "_atom",
-	version = "1.3",
+	version = "1.4",
 	dependencies = { "[drop_in_menu]" },
 	update = { id = "dropdowns", url = "https://raw.githubusercontent.com/mbien1981/dahm-modules/main/version.json" },
 })
